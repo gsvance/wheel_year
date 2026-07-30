0dcd5b79a80748f9e0b7b9a41a729d180f5ce707
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-07-30 18:03:55 -0400

    Implement JD to datetime function and remove astropy dependency

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import datetime as dt
 import enum
 import sys
 
-from astropy.time import Time
 import numpy as np
 
 
@@ -49,6 +49,43 @@ def read_table_23c():
     return a, b, c
 
 
+def jd_to_dt(jd):
+    jd += 0.5
+    z = int(jd)
+    f = jd - z
+    if z < 2299161:
+        a = z
+    else:
+        alpha = int((z - 1867216.25) / 36524.25)
+        a = z + 1 + alpha - int(alpha / 4)
+    b = a + 1524
+    c = int((b - 121.1) / 365.25)
+    d = int(365.25 * c)
+    e = int((b - d) / 30.6001)
+    day_with_decimals = b - d - int(30.6001 * e) + f
+    month = e - 1 if e < 14 else e - 13
+    year = c - 4716 if month > 2 else c - 4715
+
+    day = int(day_with_decimals)
+    day_fraction = day_with_decimals - day
+
+    hour_with_decimals = 24 * day_fraction
+    hour = int(hour_with_decimals)
+    hour_fraction = hour_with_decimals - hour
+
+    minute_with_decimals = 60 * hour_fraction
+    minute = int(minute_with_decimals)
+    minute_fraction = minute_with_decimals - minute
+
+    second_with_decimals = 60 * minute_fraction
+    second = int(second_with_decimals)
+    second_fraction = second_with_decimals - second
+
+    microsecond = round(1_000_000 * second_fraction)
+
+    return dt.datetime(year, month, day, hour, minute, second, microsecond)
+
+
 def do_computation(season, year):
     jde_0 = table_jde_0(season, year)
     t = (jde_0 - 2451545.0) / 36525
@@ -58,13 +95,14 @@ def do_computation(season, year):
     a, b_degrees, c_degrees = read_table_23c()
     s = np.sum(a * np.cos(np.radians(b_degrees + c_degrees * t)))
     jde = jde_0 + (0.00001 * s) / dl
-    time = Time(jde, format='jd')
-    time.format = 'isot'
-    return time
+    #time = Time(jde, format='jd')
+    #time.format = 'isot'
+    #return time
+    return jd_to_dt(jde)
 
 
 def main(year):
-    
+
     # Need previous winter solstice for figuring out February cross-quarter
     prev_winter = do_computation(Season.WINTER, year - 1)
 
